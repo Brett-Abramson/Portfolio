230b2a253ac6226106a75e05d9451fe0f3196f4f
Brett Abramson <brett.m.abramson@gmail.com>
2023-02-23 22:00:59 -0600

    fixed profile links.Dropdown menu working. need styling

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 
 
 
+const $mobileDropDown = $(".mobile-ul-nav");
+const $mobileHamburger = $("fa-bars");
 
 
-
-
-$newDiv = $("<div>").text("Hello World!!!")
+const $newDiv = $("<div>").text("Hello World!!!");
 
 $( () => {
 
+    $(".fa-bars").on("click", () => {
+        $(".mobile-ul-nav").toggleClass("hide");
+        console.log("button was clicked");
+    });
+    $mobileHamburger.remove();
     $("body").append($newDiv);
 })
