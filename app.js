e347b50766fc2e04ff3d556b21167a5d5ccb39c8
Brett Abramson <brett.m.abramson@gmail.com>
2023-02-28 17:46:33 -0600

    removed redundant display function

--- app.js
+++ app.js
@@ -15,14 +15,69 @@ const checkHideClass = (selector) => {
     for (let i = 0; i < $mains.length; i++) {
         if ($mains[i].hasClass("hide") !== true) {
             $mains[i].toggleClass("hide");
-            console.log($mains[i].get());
+            // console.log($mains[i].get());
         }
     };
     if (selector.hasClass("hide") === true) {
        selector.toggleClass("hide");
-        console.log(true);
+        // console.log(true);
     };
 }
+
+const displayMobile = (btnClicked) => {
+    mobileDropDown();
+    checkHideClass(btnClicked);
+}
+
+
+
+
+$( () => {
+
+
+
+    // shows/hides dropdown menu
+    $(".fa-bars").on("click", mobileDropDown);
+
+    // mobile: displays or hides website depending on "hide" class status
+    $("#li-bio-dropdown").on("click", () => {displayMobile($("#bio"))});
+    $("#li-resume-dropdown").on("click", () => {displayMobile($("#resume"))});
+    $("#li-projects-dropdown").on("click", () => {displayMobile($("#projects"))});
+    $("#li-home-dropdown").on("click", () => {displayMobile($("#home"))});
+    // desktop: displays or hides website depending on "hide" class status
+    $("#li-bio-menu").on("click", () => {checkHideClass($("#bio"))});
+    $("#li-resume-menu").on("click", () => {checkHideClass($("#resume"))});
+    $("#li-projects-menu").on("click", () => {checkHideClass($("#projects"))})
+    $("#li-home-menu").on("click", () => {checkHideClass($("#home"))});
+})
+
+
+
+// ======= why not working???? ==== am i mixing vanilla JS and jQuery?? ====
+// const displayPage = {
+//     $home: $("#home"),
+//     $bio: $("#bio"),
+//     $resume: $("#resume"),
+//     $projects: $("#projects"),
+//     checkHideClass: (selector) => {
+//         if (selector.hasClass("hide") === true) {
+//             displayPage.$home.toggleClass("hide");
+//             $("#bio").toggleClass("hide");
+//             displayPage.$resume.toggleClass("hide");
+//             displayPage.$projects.toggleClass("hide");
+//             displayPage.console.log(true);
+//         } else {
+//             console.log(false);
+//         }
+//         console.log(selector.get());
+//     },
+//     displayBio: () => {
+//         displayPage.checkHideClass(displayPage.$bio);
+//         console.log("button clicked");
+//     }
+// }
+
+
 
 // const displayBioMobile = () => {
 //     checkHideClass($("#bio"));
@@ -63,64 +118,11 @@ const checkHideClass = (selector) => {
 //     console.log("button clicked");
 // }   
 
-const display = (btnClicked) => {
-    checkHideClass(btnClicked);
-    console.log(`btnclicked`);
-}
-const displayMobile = (btnClicked) => {
-    mobileDropDown();
-    checkHideClass(btnClicked);
-}
+// const display = (btnClicked) => {
+//     checkHideClass(btnClicked);
+// }
 
-
-
-
-$( () => {
-
-
-
-    // shows/hides dropdown menu
-    $(".fa-bars").on("click", mobileDropDown);
-
-    // mobile: displays or hides website depending on "hide" class status
-    $("#li-bio-dropdown").on("click", () => {displayMobile($("#bio"))});
-    $("#li-resume-dropdown").on("click", () => {displayMobile($("#resume"))});
-    $("#li-projects-dropdown").on("click", () => {displayMobile($("#projects"))});
-    $("#li-home-dropdown").on("click", () => {displayMobile($("#home"))});
-    // desktop: displays or hides website depending on "hide" class status
     // $("#li-bio-menu").on("click", displayBio);
     // $("#li-resume-menu").on("click", displayResume);
     // $("#li-projects-menu").on("click", displayProjects)
     // $("#li-home-menu").on("click", displayHome);
-
-    $("#li-bio-menu").on("click", () => {display($("#bio"))});
-    $("#li-resume-menu").on("click", () => {display($("#resume"))});
-    $("#li-projects-menu").on("click", () => {display($("#projects"))})
-    $("#li-home-menu").on("click", () => {display($("#home"))});
-})
-
-
-
-// ======= why not working???? ==== am i mixing vanilla JS and jQuery?? ====
-// const displayPage = {
-//     $home: $("#home"),
-//     $bio: $("#bio"),
-//     $resume: $("#resume"),
-//     $projects: $("#projects"),
-//     checkHideClass: (selector) => {
-//         if (selector.hasClass("hide") === true) {
-//             displayPage.$home.toggleClass("hide");
-//             $("#bio").toggleClass("hide");
-//             displayPage.$resume.toggleClass("hide");
-//             displayPage.$projects.toggleClass("hide");
-//             displayPage.console.log(true);
-//         } else {
-//             console.log(false);
-//         }
-//         console.log(selector.get());
-//     },
-//     displayBio: () => {
-//         displayPage.checkHideClass(displayPage.$bio);
-//         console.log("button clicked");
-//     }
-// }
